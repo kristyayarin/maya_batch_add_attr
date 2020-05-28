8402d359a317db6851e322068e15f3e6fb02cebb
Xuan Huang <61477866+kristyayarin@users.noreply.github.com>
2020-05-28 03:05:09 -0400

    Update add_arnold_attrs.py

--- add_arnold_attrs.py
+++ add_arnold_attrs.py
@@ -1,22 +1,51 @@
 # -*- coding: utf-8 -*-
 import maya.cmds as cmds
 from random import uniform
+import os
 # addAttr -ln "floatattr"  -at double  -min 1 -max 4 -dv 1 |RenderManProgram1|RenderManProgram1Shape;
 
-
-mode = {
-	'const' : lambda value,min_max: value,
-	'random' :  lambda value,min_max : myRandom(min_max)
-}
+def getfiles(importPath):
+	winPath = importPath
+	fixPath = winPath.replace('\\','/')
+	paths = []
+	print(fixPath)
+	print(winPath)
+	if os.path.isfile(winPath) == True :
+		paths.append(fixPath)	
+	else :
+		for filename in os.listdir(winPath) :
+			paths.append(os.path.join(fixPath,filename))
+	print(paths)
+	
+	return paths
+	
+#____________________________________________________________________________________________
 
 def myRandom(min_max):
 
 	if (type(min_max[0]) == tuple):
 		newValue = (uniform(min_max[0][0],min_max[1][0]),uniform(min_max[0][1],min_max[1][1]),uniform(min_max[0][2],min_max[1][2]))
+	elif ( min_max[0] == 'file') :
+		paths = getfiles(min_max[1])
+		seed = int(uniform( 0, len(paths) ))
+		newValue = paths[seed]
 	else:
 		newValue = uniform(min_max[0], min_max[1])
 	return newValue
+#===========================================================================================
+'''
+def myLiner(min_max):
+	if (type(min_max[0]) == ):
 	
+ '''
+#____________________________________________________________________________________________
+
+mode = {
+	'const' : lambda value,min_max, step : value,
+	'random' :  lambda value,min_max, step : myRandom(min_max),
+	#'liner' : lambda value, min_max, step : myLiner(min_max, step)
+}
+
 #===========================================================================================	
 def add_float(shape, attrname, value, min_max):	
 	if cmds.attributeQuery(attrname, node=shape, exists=True) == False:
@@ -52,17 +81,17 @@ def add_color(shape, attrname, value, min_max = 'none'):
 		cmds.setAttr(shape + '.' + attrname + 'R', value[0])
 		cmds.setAttr(shape + '.' + attrname + 'G', value[1])
 		cmds.setAttr(shape + '.' + attrname + 'B', value[2])
-	
-
-attrType = {
-	'float' : add_float,
-	'int' : add_int,
-	'color' : add_color
-}
 
 #===========================================================================================
 
+def add_file(shape, attrname, value, min_max):
+	if cmds.attributeQuery(attrname , node=shape, exists=True) == False:
+		cmds.addAttr(shape, ln=attrname, sn=attrname, nn = attrname, dt='string')
+		cmds.setAttr(shape + '.' + attrname, value, typ='string')
+	else:
+		cmds.setAttr(shape + '.' + attrname, value, typ='string')
 
+#===========================================================================================
 def deleteAttr(attrname):
 	selections = cmds.ls(sl=True)
 	shapes = cmds.listRelatives(selections)
@@ -71,6 +100,14 @@ def deleteAttr(attrname):
 			cmds.deleteAttr(shape, at = attrname)
 
 #===========================================================================================
+
+attrType = {
+	'float' : add_float,
+	'int' : add_int,
+	'color' : add_color,
+	'file' : add_file
+}
+
 
 def batchAddAttr(dataType, attributename, defult_value, modeType, min_max, Ai):
 
@@ -82,16 +119,17 @@ def batchAddAttr(dataType, attributename, defult_value, modeType, min_max, Ai):
 
 	selections = cmds.ls(sl=True)
 	shapes = cmds.listRelatives(selections)
+	
 
 	if len(selections) == 0:
 			print('Cannot find a shape to add attribute')
 	if shapes == None:
 		shapes = selections
-
-	value = mode.get(modeType)(defult_value,min_max)
-
+		
+	step = len(shapes)
+	
 	for obj in shapes:
-		value = mode.get(modeType)(defult_value,min_max)
+		value = mode.get(modeType)(defult_value,min_max, step)
 		attrType.get(dataType)(obj, attrname, value, min_max)
 	
 
